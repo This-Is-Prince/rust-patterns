c40b08309dc1ad6fe55319561b18959e9ef726c8
This-Is-Prince <this.is.kumar.prince@gmail.com>
2023-07-07 14:16:30 +0530

    first commit

---
+++ target/debug/deps/rust_patterns-9cfdd5e67e13ad3b.d
@@ -0,0 +1,5 @@
+/Users/prince/Web Development/Rust/rust-patterns/target/debug/deps/rust_patterns-9cfdd5e67e13ad3b.rmeta: src/main.rs
+
+/Users/prince/Web Development/Rust/rust-patterns/target/debug/deps/rust_patterns-9cfdd5e67e13ad3b.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/rust_patterns-e342ad3c6ee886e0.d
@@ -0,0 +1,5 @@
+/Users/prince/Web Development/Rust/rust-patterns/target/debug/deps/rust_patterns-e342ad3c6ee886e0.rmeta: src/main.rs
+
+/Users/prince/Web Development/Rust/rust-patterns/target/debug/deps/rust_patterns-e342ad3c6ee886e0.d: src/main.rs
+
+src/main.rs:
