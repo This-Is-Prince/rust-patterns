437ed7978a59143d4ef1a7c3728f61dda288c665
This-Is-Prince <this.is.kumar.prince@gmail.com>
2023-07-07 14:20:55 +0530

    feat: first pattern complete

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    println!("---- Star Patterns ----");
+    first();
+}
+
+fn first() {
+    let row = 5;
+    let col = 5;
+    for _ in 0..row {
+        for _ in 0..col {
+            print!("*");
+        }
+        println!();
+    }
 }
