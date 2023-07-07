447a275c3b8699bc14399804773cdc8f463a51cf
This-Is-Prince <this.is.kumar.prince@gmail.com>
2023-07-07 14:30:59 +0530

    feat: 5 pattern complete

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 fn main() {
     println!("---- Star Patterns ----");
     first();
+    second();
+    third();
+    fourth();
+    fifth();
 }
 
 fn first() {
+    println!("1. First Pattern\n");
     let row = 5;
     let col = 5;
     for _ in 0..row {
@@ -12,4 +17,65 @@ fn first() {
         }
         println!();
     }
+    println!("\n")
+}
+
+fn second() {
+    println!("2. Second Pattern\n");
+    let row = 5;
+    let col = 5;
+    for i in 0..row {
+        for j in 0..col {
+            if j <= i {
+                print!("*")
+            }
+        }
+        println!();
+    }
+    println!("\n")
+}
+
+fn third() {
+    println!("3. Third Pattern\n");
+    let row = 5;
+    let col = 5;
+    for i in 1..=row {
+        for j in 1..=col {
+            if j <= i {
+                print!("{j}")
+            }
+        }
+        println!();
+    }
+    println!();
+}
+
+fn fourth() {
+    println!("4. Fourth Pattern\n");
+    let row = 5;
+    let col = 5;
+    for i in 1..=row {
+        for j in 1..=col {
+            if j <= i {
+                print!("{i}")
+            }
+        }
+        println!();
+    }
+    println!();
+}
+
+fn fifth() {
+    println!("5. Fifth Pattern\n");
+    let row = 5;
+    let col = 5;
+    for i in 1..=row {
+        for j in 1..=col {
+            if j <= col + 1 - i {
+                print!("*")
+            }
+        }
+        println!();
+    }
+    println!();
 }
